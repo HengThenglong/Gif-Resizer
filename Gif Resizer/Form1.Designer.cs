53a37413058cd420e53cf01caa3945105dbf7db6
Heng-Thenglong <49927604+Heng-Thenglong@users.noreply.github.com>
2020-06-07 04:10:43 +0700

    Add Coverted Folder

--- Gif Resizer/Form1.Designer.cs
+++ Gif Resizer/Form1.Designer.cs
@@ -28,22 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnConvert = new System.Windows.Forms.Button();
+            this.BtnOpenFile = new System.Windows.Forms.Button();
+            this.PicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // BtnConvert
+            // 
+            this.BtnConvert.Enabled = false;
+            this.BtnConvert.Location = new System.Drawing.Point(637, 305);
+            this.BtnConvert.Name = "BtnConvert";
+            this.BtnConvert.Size = new System.Drawing.Size(145, 59);
+            this.BtnConvert.TabIndex = 0;
+            this.BtnConvert.Text = "Convert";
+            this.BtnConvert.UseVisualStyleBackColor = true;
+            this.BtnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // BtnOpenFile
+            // 
+            this.BtnOpenFile.Location = new System.Drawing.Point(637, 228);
+            this.BtnOpenFile.Name = "BtnOpenFile";
+            this.BtnOpenFile.Size = new System.Drawing.Size(145, 59);
+            this.BtnOpenFile.TabIndex = 1;
+            this.BtnOpenFile.Text = "Open File";
+            this.BtnOpenFile.UseVisualStyleBackColor = true;
+            this.BtnOpenFile.Click += new System.EventHandler(this.BtnOpenFile_Click);
+            // 
+            // PicBox
+            // 
+            this.PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicBox.Location = new System.Drawing.Point(26, 12);
+            this.PicBox.Name = "PicBox";
+            this.PicBox.Size = new System.Drawing.Size(546, 352);
+            this.PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicBox.TabIndex = 2;
+            this.PicBox.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(811, 388);
+            this.Controls.Add(this.PicBox);
+            this.Controls.Add(this.BtnOpenFile);
+            this.Controls.Add(this.BtnConvert);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnConvert;
+        private System.Windows.Forms.Button BtnOpenFile;
+        private System.Windows.Forms.PictureBox PicBox;
     }
 }
 
--- Gif Resizer/Form1.cs
+++ Gif Resizer/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ImageMagick;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,52 @@ namespace Gif_Resizer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        FileInfo File;
+        private void BtnOpenFile_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Title = "Pick an Animated Gif",
+                Filter = "Gif Image file (*.gif)|*.gif",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PicBox.Image = new Bitmap(openFileDialog.FileName);
+                File = new FileInfo(openFileDialog.FileName);
+                BtnConvert.Enabled = true;
+            }
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+
+
+
+            // Read from file
+            using (var collection = new MagickImageCollection(File))
+            {
+                // This will remove the optimization and change the image to how it looks at that point
+                // during the animation. More info here: http://www.imagemagick.org/Usage/anim_basics/#coalesce
+                collection.Coalesce();
+
+                // Resize each image in the collection to a width of 200. When zero is specified for the height
+                // the height will be calculated with the aspect ratio.
+                foreach (var image in collection)
+                {
+                    image.Resize(200, 200);
+                }
+
+                Directory.CreateDirectory(File.Directory + "\\Converted");
+                File = new FileInfo($"{File.Directory}\\Converted\\{File.Name}");
+                // Save the result
+                //collection.Write($"{File}_Convert.gif");
+                collection.Write(File);
+                
+            }
         }
     }
 }
